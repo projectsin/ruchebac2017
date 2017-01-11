f7edb5e1eba29b5d8d0905b3e9974ee13f6f1442
projectsin <emilien-apple@outlook.com>
2017-01-11 10:01:12 +0000

    Update network.ino

--- network.ino
+++ network.ino
@@ -2,20 +2,41 @@
 
 SoftwareSerial xbee(2, 3); // RX, TX
 
+//protocole d'envoie id;masse;temperature;hygrometrie/
+
+byte id = 1; //TODO : definir ensuite depuis l"exterieur
+
 void setup()  {
   Serial.begin(9600);
   xbee.begin(9600);
 }
 
 void loop()  {
-  while (Serial.available()) {
-    String data = Serial.readString();
-    xbee.print(data);
-    Serial.println("Envoie -> " + data);
-  }
-
   while (xbee.available()) {
-    Serial.println("Reception <- " + xbee.readString());
+    if(xbee.read() == 'P') 
+      sendData();
   }
  
 }
+
+void sendData() {
+  send(String(id) + ";" + String(getMasse()) + ";" + String(getTemperature()) + ";" + String(getHygrometrie()) + "/";
+}
+       
+void send(String data) {
+  xbee.print(data);
+}  
+       
+short getMasse() {
+  return 0;
+}
+       
+short getTemperature() {
+  return 0; 
+}
+       
+short getHygrometrie() {
+  return 0; 
+}
+       
+
