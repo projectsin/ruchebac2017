6206683e146310622524bfcb5712416b18212b6c
projectsin <emilien-apple@outlook.com>
2017-01-11 09:43:50 +0000

    Add files via upload

---
+++ network.ino
@@ -0,0 +1,27 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial xbee(2, 3); // RX, TX
+char c = 'A';
+int  pingPong = 1;
+
+void setup()  {
+  Serial.begin(9600);
+  Serial.println( "Arduino started sending bytes via XBee" );
+
+  xbee.begin(9600);
+}
+
+void loop()  {
+  while (Serial.available()) {
+    String data = Serial.readString();
+    xbee.print(data);
+    Serial.println("Envoie -> " + data);
+  }
+
+  while (xbee.available()) {
+    Serial.println("Reception <- " + xbee.readString());
+  }
+  
+
+
+}
