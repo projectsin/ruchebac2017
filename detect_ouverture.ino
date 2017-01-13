2ace4bc6f871523c277226293689f8f905685fb3
projectsin <emilien-apple@outlook.com>
2017-01-13 10:45:20 +0000

    Add files via upload

---
+++ detect_ouverture.ino
@@ -0,0 +1,17 @@
+#define PIN_REED_SENSOR 2
+int pinReedSensor;
+int oldPinReedSensor;
+void setup() {
+    Serial.begin(9600);
+    Serial.println("*** Setup ***");
+    pinMode(PIN_REED_SENSOR, INPUT);
+    oldPinReedSensor = digitalRead(PIN_REED_SENSOR);
+}
+void loop() {
+    pinReedSensor = digitalRead(PIN_REED_SENSOR);
+    if (pinReedSensor != oldPinReedSensor) {
+        Serial.print("Reed = ");
+        Serial.println(pinReedSensor);
+        oldPinReedSensor = pinReedSensor;
+    }
+}
