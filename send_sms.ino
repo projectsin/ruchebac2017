23d738d8895cc5ab5223bb315580ec7c46594873
projectsin <emilien-apple@outlook.com>
2017-01-20 10:59:57 +0000

    Add files via upload

--- send_sms.ino
+++ send_sms.ino
@@ -3,44 +3,53 @@
 
 #define PINNUMBER "1234"
 
-//SoftwareSerial xbee(2, 3);
 
-GSM gsmAccess;
-GSM_SMS sms;
+class SmsSender {
+  public:
+  GSM gsmAccess;
+  GSM_SMS sms;
+  
+  void send_sms(String msg){
+   boolean notConnected = true;
+   Serial.println("Connection...");
+    while (notConnected) {
+      if (gsmAccess.begin(PINNUMBER) == GSM_READY) {
+       notConnected = false;
+      } else {
+       Serial.println("Not connected");
+       delay(1000);
+     }
+    }
+    Serial.println("Envoie...");
+    sms.beginSMS("0611511932");
+    sms.print(msg);
+    sms.endSMS();
+    Serial.println("Message envoyé !");
+  }
+};
+
+#ifndef Morse_h
+#define Morse_h
+
+class XbeeSender {
+  #include <SoftwareSerial.h>
+  SoftwareSerial xbee(2, 3);
+  xbee.begin(9600);
+  public:
+  void sendData(String data){
+    xbee.print(data);
+    Serial.println(data);
+  }
+  
+};
+
+#endif
+
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial) {
-    ;
-  }
-  send_sms("Test4");
-  //xbee.begin(9600);
-
-  //sendData("Test xbee");
 }
 
 void loop() {
 }
 
-/*void sendData(String data){ //test
-  xbee.print(data);
-  Serial.println(data);
-}*/
-
-void send_sms(String msg){
-  boolean notConnected = true;
-  Serial.println("Connection...");
-  while (notConnected) {
-    if (gsmAccess.begin(PINNUMBER) == GSM_READY) {
-      notConnected = false;
-    } else {
-      Serial.println("Not connected");
-      delay(1000);
-    }
-  }
-  Serial.println("Envoie...");
-  sms.beginSMS("0611511932");
-  sms.print(msg);
-  sms.endSMS();
-  Serial.println("Message envoyé !");
-}
