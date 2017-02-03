93dc06963eed15251ee68716129cb6d717ce7b44
projectsin <emilien-apple@outlook.com>
2017-02-03 10:53:40 +0000

    Update centrale.ino

--- centrale.ino
+++ centrale.ino
@@ -3,12 +3,22 @@
 
 GSM gsmAccess;
 GSM_SMS sms;
+GSMVoiceCall vcs;
 
 boolean alert = false;
 
 void setup() {
   Serial.begin(9600);
   Serial1.begin(9600);
+  boolean notConnected = true;
+  while (notConnected) {
+    if (gsmAccess.begin(PINNUMBER) == GSM_READY) {
+      notConnected = false;
+    } else {
+      Serial.println("Not connected");
+      delay(1000);
+    }
+  }
 }
 
 void loop() {
@@ -20,22 +30,29 @@ void loop() {
   }
   while (Serial1.available()) {
     Serial.println("Reception <- " + Serial1.readString());
-    
   }
 }
 void sendSms(String msg)
 {
-  boolean notConnected = true;
-  while (notConnected) {
-    if (gsmAccess.begin(PINNUMBER) == GSM_READY) {
-      notConnected = false;
-    } else {
-      delay(1000);
-    }
-  }
   sms.beginSMS("0611511932");
   sms.print(msg);
   sms.endSMS();
+}
+
+void callGsm(){
+   String remoteNumber = "0611511932"; 
+   char charbuffer[20];
+   if (remoteNumber.length() < 20) {
+      remoteNumber.toCharArray(charbuffer, 20);
+      if (vcs.voiceCall(charbuffer)) {
+         while (Serial.read() != '\n' && (vcs.getvoiceCallStatus() == TALKING));
+         vcs.hangCall();
+      }
+        remoteNumber = "";
+    } else {
+       Serial.println("That's too long for a phone number. I'm forgetting it");
+       remoteNumber = "";
+    }
 }
 
 String protocol(String id, String masse, String temperature, String humidity) {
