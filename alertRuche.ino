670d529d5c85ab31dc3b7f70abf3188766da5f26
projectsin <emilien-apple@outlook.com>
2017-03-15 10:00:07 +0000

    Update alertRuche.ino

--- alertRuche.ino
+++ alertRuche.ino
@@ -1,6 +1,11 @@
 #define detect 2
 #include <SoftwareSerial.h>
 int etatDetect;
+int hygrometrie, temperature, masse;
+int intervalleHygrometrie[] = {5,20};
+int intervalleTemperature[] = {5,20};
+int intervalleMasse[] = {5,20};
+
 boolean sendAlert = true;
 
 SoftwareSerial xbee(2, 3);
@@ -13,7 +18,7 @@ void setup() {
 void loop() {
   etatDetect = digitalRead(detect);
   if(sendAlert && etatDetect == 0){
-    xbee.print("Alerte");
+    xbee.print("AlerteVol");
     sendAlert = false;
   }
   if(!sendAlert && etatDetect == 1){
