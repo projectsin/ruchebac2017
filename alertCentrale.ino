0c8e37ccce70fafd9d254e62a398a8464b7c1147
projectsin <emilien-apple@outlook.com>
2017-03-15 09:16:38 +0000

    Update alertCentrale.ino

--- alertCentrale.ino
+++ alertCentrale.ino
@@ -2,16 +2,22 @@ boolean activatedAlert = true; //Modifiable via l'ihm
 
 void setup() {
     Serial.begin(9600);
+    Serial1.begin(9600);
 }
 void loop() {
-    while(Serial1.available()){
-      if(Serial1.read() == "Alerte" && activatedAlert == true){
-         sendAlertToOperator();
-         activatedAlert = false;
-      }
-    }
+    checkAlert();
 }
 
 void sendAlertToOperator(){
-    
+    //Envois sms et appel opérateur
 }
+
+void checkAlert(){
+    while(Serial1.available()){
+        if(Serial1.read() == "Alerte" && activatedAlert == true){
+            sendAlertToOperator();
+            activatedAlert = false;
+        }
+    }
+}
+  
