3b6376e7db56c88f19b5fded851ec818f0cb078d
projectsin <emilien-apple@outlook.com>
2017-03-15 07:54:54 +0000

    Add files via upload

---
+++ ouverture.ino
@@ -0,0 +1,15 @@
+#define detect 2
+int etatDetect;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(detect, INPUT);
+
+}
+
+void loop() {
+  etatDetect = digitalRead(detect);
+  Serial.println(etatDetect);
+  delay(200);
+
+}
