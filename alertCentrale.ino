b7d5941f579ec9d8fa7acd6349905121c39c7c96
projectsin <emilien-apple@outlook.com>
2017-03-22 07:20:00 +0000

    Update alertCentrale.ino

--- alertCentrale.ino
+++ alertCentrale.ino
@@ -6,40 +6,52 @@ GSM_SMS sms;
 GSMVoiceCall vcs;
 
 boolean activatedAlert = true; //Modifiable via l'ihm
-String number = "0611511932"; //Modifiable via l'ihm
+int idRuche = 1; 
+String packet = "!Alert#Vol#1";
+String subPacket;
 
 void setup() {
     Serial.begin(9600);
     Serial1.begin(9600);
     boolean notConnected = true;
-    while (notConnected) {
+    /*while (notConnected) {
         if (gsmAccess.begin(PINNUMBER) == GSM_READY) {
             notConnected = false;
         } else {
             Serial.println("Not connected");
             delay(1000);
         }
-    }
+    }*/
 }
 void loop() {
     checkAlert();
 }
 
-void sendAlertToOperator(){
-    callGsm();
-}
-
 void checkAlert(){
     while(Serial1.available()){
-        if(Serial1.readString() == "AlerteVol" && activatedAlert == true){
-            sendAlertToOperator();
-            activatedAlert = false;
+        Serial.println(Serial1.readString());
+        if(Serial1.readString() == "!Alert#" && activatedAlert == true){
+            packet = Serial1.readString();
+            Serial.println(packet);
+            if(packet.substring(0,7) == "!Alert#"){
+              subPacket = packet.substring(7,10);
+              if(subPacket == "Vol"){
+                sendSms("Alerte > Vol (id=" + packet.substring(11) + ")");
+                //callGsm();
+              }
+      
+              if(subPacket == "Tem") sendSms("Alerte > Température (" + packet.substring(19) + "°C)");
+              if(subPacket == "Hyg") sendSms("Alerte > Hygrométrie (" + packet.substring(19) + "%)");
+              if(subPacket == "Bat") sendSms("Alerte > Niveau de batterie (" + packet.substring(16) + "%)");
+             } 
         }
     }
+    delay(200);
+    
 }
 
 void callGsm(){
-   String remoteNumber = number;
+   String remoteNumber = "0611511932"; 
    char charbuffer[20];
    if (remoteNumber.length() < 20) {
       remoteNumber.toCharArray(charbuffer, 20);
@@ -53,4 +65,7 @@ void callGsm(){
        remoteNumber = "";
     }
 }
-  
+
+void sendSms(String msg){
+  Serial.println(msg);
+}
