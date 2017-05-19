486023181ac45dde39084b43186fb0081751aeab
projectsin <emilien-apple@outlook.com>
2017-05-19 08:49:36 +0100

    Add files via upload

--- test_sd.ino
+++ test_sd.ino
@@ -1,62 +1,61 @@
 /*SD Card --> BUS SPI:
-  MOSI -> pin 11
+  MOSI -> pin 11, uno
   MISO -> pin 12
   SCK -> pin 13
-  CS -> pin 4 */
+  CS -> pin 53 */
 
-
+#include <Wire.h>
+#include <DS3231.h>
 #include <SD.h>
 #include <SPI.h>
-
+DS3231 clock;
+RTCDateTime dt;
 File fileSD;
 
 
 void setup() {
   Serial.begin(9600);
-  if (!SD.begin(4)) {
+  Serial.flush();
+  Serial.println("Initialize DS3231");
+  clock.begin();
+  clock.setDateTime(__DATE__, __TIME__); // La RTC prend l'heure de l'ordinateur
+  pinMode(19, INPUT_PULLUP);
+  if (!SD.begin(53)) {
     Serial.println("initialization failed");
     return;
   }
   Serial.println("initialization done");
 }
 
-/*Pour plus tard #SauvegardeSD
-    Serial.println(t.year);
-    Serial.println(t.month);
-    Serial.println(t.day);
-    Serial.println(t.DayOfWeek);
-    Serial.println(t.hour);
-    Serial.println(t.minute); */
 
 void loop() {
   fileSD = SD.open("fileSD.txt", FILE_WRITE);
-   int masse = random(101,200);
+  int masse = random(101, 200);
   int temp = random(10, 70);
-   int hygro = random(0, 100);
-
+  int hygro = random(0, 100);
+  dt = clock.getDateTime(); //Prend l'heure
+  if (SD.exists("fileSD.txt")) {
+    ;
+  } else {
+    fileSD.print("Date");
+    fileSD.print(",");
+    fileSD.print("Masse (Kg)");
+    fileSD.print(",");
+    fileSD.print("Temperature (*C)");
+    fileSD.print(",");
+    fileSD.println("Hygrometrie (/100)");
+  }
   if (fileSD) {
     Serial.println("ecriture");
+    //fileSD.print(",");  Ecriture de "," dans le fichier. Ce caractère permet de séparer les données pour ranger les données dans le tableur
+    fileSD.print(clock.dateFormat("d-m-Y H:i:s - l", dt));
     fileSD.print(",");
-    fileSD.print("Date / Heure");
-    fileSD.print(t.year);
-    fileSD.print(";");
-    fileSD.println(t.month);
-    fileSD.print(";");
-    fileSD.println(t.day);
-    fileSD.print(";");
-    fileSD.println(t.hour);
-    fileSD.print(":");
-    fileSD.println(t.minute);
-    fileSD.print(","); //Ecriture de "," dans le fichier. Ce caractère permet de séparer les données pour ranger les données dans le tableur
-    fileSD.print("Masse : ");
     fileSD.print(masse);
     fileSD.print(",");
-    fileSD.print("temperature : ");
     fileSD.print(temp);
     fileSD.print(",");
-     fileSD.print("hygrometrie : ");
     fileSD.println(hygro);
-    //fileSD.print(",");
+    fileSD.println(" ");
     fileSD.close();
     Serial.println("done");
   } else {
@@ -65,7 +64,7 @@ void loop() {
 
 
 
-  
-  }
+
+}
 
 
